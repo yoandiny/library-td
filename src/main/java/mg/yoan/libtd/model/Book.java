2b05037e7c88d727def9368bd9cb8b9541f596df
Yoan <yoandiny14@gmail.com>
2026-06-08 09:39:08 +0300

    Add Book entity with JPA repository and PostgreSQL configuration

---
+++ src/main/java/mg/yoan/libtd/model/Book.java
@@ -0,0 +1,34 @@
+package mg.yoan.libtd.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String author;
+
+  @Column(unique = true)
+  private String isbn;
+}
