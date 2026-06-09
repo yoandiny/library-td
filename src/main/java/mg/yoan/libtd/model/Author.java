b9bb646093020278940e96c2535d6e98fabeff24
Yoan <yoandiny14@gmail.com>
2026-06-09 13:48:08 +0300

    Add Author entity with one-to-many relationship to Book and include additional Book fields

--- src/main/java/mg/yoan/libtd/model/Author.java
+++ src/main/java/mg/yoan/libtd/model/Author.java
@@ -2,43 +2,37 @@ package mg.yoan.libtd.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-@Table(name = "book")
+@Table(name = "author")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Book {
+public class Author {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
-  private String title;
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "author_id", nullable = false)
-  private Author author;
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
 
-  @Column(unique = true)
-  private String isbn;
-
-  @Column(name = "publication_year")
-  private Integer publicationYear;
-
-  @Column
-  private String genre;
+  @ToString.Exclude
+  @OneToMany(mappedBy = "author")
+  private List<Book> books;
 }
---
+++ src/main/java/mg/yoan/libtd/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package mg.yoan.libtd.repository;
+
+import mg.yoan.libtd.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {}
