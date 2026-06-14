03318b55a02d3ebea8427b32fdf547cc2ad03d6f
Rel4w <hei.jonathan.4@gmail.com>
2026-06-14 21:44:20 +0300

    feat(repository): add SaleRepository and SaleLineRepository

---
+++ src/main/java/mg/yoan/libtd/repository/SaleRepository.java
@@ -0,0 +1,14 @@
+package mg.yoan.libtd.repository;
+
+import java.util.List;
+import mg.yoan.libtd.model.Sale;
+import mg.yoan.libtd.model.SaleStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, String> {
+    List<Sale> findAllByCustomerId(String customerId);
+
+    List<Sale> findAllByStatus(SaleStatus status);
+}
