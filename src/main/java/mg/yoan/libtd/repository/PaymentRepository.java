37297d14410fa7e92cc87e159f42987d26284644
Rel4w <hei.jonathan.4@gmail.com>
2026-06-14 21:45:14 +0300

    feat(repository): add PaymentRepository

---
+++ src/main/java/mg/yoan/libtd/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package mg.yoan.libtd.repository;
+
+import java.util.Optional;
+import mg.yoan.libtd.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+    Optional<Payment> findBySaleId(String saleId);
+}
