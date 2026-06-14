68cc27e20cd05907e40cdf776314c8ef9ee05462
Rel4w <hei.jonathan.4@gmail.com>
2026-06-14 21:55:35 +0300

    feat: add Customer entity , customer repository and customer table

---
+++ src/main/resources/db/migration/V42_5__Create_customer_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS customer
+(
+    id          VARCHAR DEFAULT gen_random_uuid()
+        CONSTRAINT customer_pk PRIMARY KEY,
+    full_name   VARCHAR NOT NULL,
+    email       VARCHAR NOT NULL UNIQUE,
+    phone       VARCHAR,
+    register_at TIMESTAMP
+);
