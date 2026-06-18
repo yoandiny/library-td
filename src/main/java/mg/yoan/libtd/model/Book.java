6c3c7c22fc86bcdec8dd3d59cfa4475fd04ff327
Rindra <hei.rindra.2@gmail.com>
2026-06-18 15:47:12 +0300

    delete: manyToOne library

--- src/main/java/mg/yoan/libtd/model/Book.java
+++ src/main/java/mg/yoan/libtd/model/Book.java
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 public class Book {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.UUID)
   private String id;
 
   @Column(nullable = false)
