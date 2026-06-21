3b7ea627d533e44104b32342d262a9159a62c0d6
Rindra <hei.rindra.2@gmail.com>
2026-06-21 22:25:10 +0300

    feat : create record error response

---
+++ src/main/java/mg/yoan/libtd/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package mg.yoan.libtd.exception;
+
+import java.time.Instant;
+
+public record ErrorResponse(
+        Instant timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+) {
+}
