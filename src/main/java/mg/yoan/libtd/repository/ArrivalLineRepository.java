05501b3a33973e23241f743cef8ede25231f0b64
Rindra <hei.rindra.2@gmail.com>
2026-06-21 22:53:17 +0300

    fix: add @Repository

--- src/main/java/mg/yoan/libtd/repository/ArrivalLineRepository.java
+++ src/main/java/mg/yoan/libtd/repository/ArrivalLineRepository.java
@@ -2,9 +2,11 @@ package mg.yoan.libtd.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ArrivalLineRepository {
     @Query("SELECT COALESCE(SUM(al.quantity), 0) FROM ArrivalLine al WHERE al.bookEdition.id = :bookEditionId")
     Integer sumQuantityByBookEditionId(@Param("bookEditionId") UUID bookEditionId);
