20197c547123f01500cb69a6c514e7a1fc8e92c1
Rindra <hei.rindra.2@gmail.com>
2026-06-21 23:07:36 +0300

    test: add StockController web layer tests

---
+++ src/test/java/mg/yoan/libtd/controller/StockControllerTest.java
@@ -0,0 +1,45 @@
+package mg.yoan.libtd.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.UUID;
+
+import mg.yoan.libtd.endpoint.rest.controller.health.StockController;
+import mg.yoan.libtd.service.StockService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(StockController.class)
+class StockControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    StockService stockService;
+
+    @Test
+    void getStock_returns200WithStock() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(stockService.getStock(id)).thenReturn(7);
+
+        mockMvc.perform(get("/stock/book-edition/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(content().string("7"));
+    }
+
+    @Test
+    void getStock_withUnknownId_stillReturns200WithZero() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(stockService.getStock(id)).thenReturn(0);
+
+        mockMvc.perform(get("/stock/book-edition/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(content().string("0"));
+    }
+}
