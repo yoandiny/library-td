2c9e799d85cb4253064fb22d6626bf2515dc946b
Rindra <hei.rindra.2@gmail.com>
2026-06-22 08:44:49 +0300

    fix: extend jpaRepository

--- src/main/java/mg/yoan/libtd/repository/ArrivalLineRepository.java
+++ src/main/java/mg/yoan/libtd/repository/ArrivalLineRepository.java
@@ -1,12 +1,14 @@
 package mg.yoan.libtd.repository;
 
 import java.util.UUID;
+import mg.yoan.libtd.model.ArrivalLine;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArrivalLineRepository {
+public interface ArrivalLineRepository extends JpaRepository<ArrivalLine, UUID> {
   @Query(
       "SELECT COALESCE(SUM(al.quantity), 0) FROM ArrivalLine al WHERE al.bookEdition.id ="
           + " :bookEditionId")
